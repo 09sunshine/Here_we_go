7366c1230f0af37a894f532143dec8af2da55c11
Arshsingh1602 <arsharya1604@gmail.com>
2025-08-25 13:16:46 +0530

    user

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,28 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import { Student } from "./models/student.js";
 
-dotenv.config({
-    path: "./.env"
-});
+
 const app = express()
+
+app.use(express.json());
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }));
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running on localhost!');
+});
+
+app.post('/api/students', async (req, res) => {
+    try {
+        const student = new Student(req.body);
+        await student.save();
+        res.status(201).json({ message: "Student created successfully", student });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 export {app}
